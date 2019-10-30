33d4329e1ced1440a2e9b4a94c4d746dc0645fd8
王铎 <956389596@qq.com>
2019-10-30 17:41:12 +0800

    底部Tabbar-基本配置

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // 通过Home中的三个Link导航切换三个组件显示与隐藏
 import { Link, Route } from 'react-router-dom'
+import { TabBar } from 'antd-mobile';
 
 // Index
 // HouseList
@@ -10,21 +11,98 @@ import Index from '../../pages/Index/index.js'
 import HouseList from '../../pages/HouseList/index.js'
 import Profile from '../../pages/Profile/index.js'
 
+// 首页
+// 找房
+// 我的
 class Home extends React.Component {
-    componentDidMount() {}
+    state = {
+        selectedTab: 'blueTab',
+    }
+
     render() {
         return (
-            <div>
-                <Route path="/home/index" component={Index}></Route>
-                <Route path="/home/house" component={HouseList}></Route>
-                <Route path="/home/profile" component={Profile}></Route>
-                <hr />
-                <div>
-                    <Link to="/home/index">首页</Link><br />
-                    <Link to="/home/house">房屋列表</Link><br />
-                    <Link to="/home/profile">个人中心</Link>
-                </div>
-            </div>
+            <div
+                style={{ position: 'fixed', height: '100%', width: '100%', top: 0}}>
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#33A3F4"
+          barTintColor="white"
+        >
+          <TabBar.Item
+            title="首页"
+            key="Life"
+            icon={<div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+            />
+            }
+            selectedIcon={<div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+            />
+            }
+            selected={this.state.selectedTab === 'blueTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'blueTab',
+              });
+            }}
+            data-seed="logId"
+          >
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            title="找房"
+            key="Koubei"
+            selected={this.state.selectedTab === 'redTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'redTab',
+              });
+            }}
+          >
+          </TabBar.Item>
+          <TabBar.Item
+            icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+            selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+            title="我的"
+            key="my"
+            selected={this.state.selectedTab === 'yellowTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'yellowTab',
+              });
+            }}
+          >
+          </TabBar.Item>
+        </TabBar>
+      </div>
+            // <div>
+            //     <Route path="/home/index" component={Index}></Route>
+            //     <Route path="/home/house" component={HouseList}></Route>
+            //     <Route path="/home/profile" component={Profile}></Route>
+            //     <hr />
+            //     <div>
+            //         <Link to="/home/index">首页</Link><br />
+            //         <Link to="/home/house">房屋列表</Link><br />
+            //         <Link to="/home/profile">个人中心</Link>
+            //     </div>
+            // </div>
         )
     }
 }
