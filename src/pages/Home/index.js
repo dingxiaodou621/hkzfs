9aac65e750b2621caecd2c47f488d233c5310ed3
王铎 <956389596@qq.com>
2019-10-30 21:07:11 +0800

    底部Tabbar-调整位置-字体图标

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,6 +11,9 @@ import Index from '../../pages/Index/index.js'
 import HouseList from '../../pages/HouseList/index.js'
 import Profile from '../../pages/Profile/index.js'
 
+import '../../assets/fonts/iconfont.css'
+import './index.css'
+
 // 首页
 // 找房
 // 我的
@@ -21,9 +24,8 @@ class Home extends React.Component {
 
     render() {
         return (
-            <div
-                style={{ position: 'fixed', height: '100%', width: '100%', top: 0}}>
-        <TabBar
+          <div className="tabbar">
+            <TabBar
           unselectedTintColor="#949494"
           tintColor="#33A3F4"
           barTintColor="white"
@@ -31,18 +33,8 @@ class Home extends React.Component {
           <TabBar.Item
             title="首页"
             key="Life"
-            icon={<div style={{
-              width: '22px',
-              height: '22px',
-              background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
-            />
-            }
-            selectedIcon={<div style={{
-              width: '22px',
-              height: '22px',
-              background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
-            />
-            }
+            icon={<i className="iconfont icon-ind" />}
+            selectedIcon={<i className="iconfont icon-ind" />}
             selected={this.state.selectedTab === 'blueTab'}
             onPress={() => {
               this.setState({
@@ -53,20 +45,8 @@ class Home extends React.Component {
           >
           </TabBar.Item>
           <TabBar.Item
-            icon={
-              <div style={{
-                width: '22px',
-                height: '22px',
-                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
-              />
-            }
-            selectedIcon={
-              <div style={{
-                width: '22px',
-                height: '22px',
-                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
-              />
-            }
+            icon={<i className="iconfont icon-findHouse" />}
+            selectedIcon={<i className="iconfont icon-findHouse" />}
             title="找房"
             key="Koubei"
             selected={this.state.selectedTab === 'redTab'}
@@ -78,8 +58,8 @@ class Home extends React.Component {
           >
           </TabBar.Item>
           <TabBar.Item
-            icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
-            selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+            icon={<i className="iconfont icon-my" />}
+            selectedIcon={<i className="iconfont icon-my" />}
             title="我的"
             key="my"
             selected={this.state.selectedTab === 'yellowTab'}
@@ -91,7 +71,7 @@ class Home extends React.Component {
           >
           </TabBar.Item>
         </TabBar>
-      </div>
+          </div>
             // <div>
             //     <Route path="/home/index" component={Index}></Route>
             //     <Route path="/home/house" component={HouseList}></Route>
