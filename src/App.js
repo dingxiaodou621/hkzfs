a66a1ffe6e32d77c641482aa113113c94e9dcb88
王铎 <956389596@qq.com>
2019-10-30 16:25:29 +0800

    配置基础路由并设置全局样式

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react'
+// import { Button } from 'antd-mobile'
+
+// 在这个文件写路由代码
+
+import Home from './pages/Home/index.js'
+import Map from './pages/Map/index.js'
+
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+
 class App extends React.Component {
   componentDidMount() { }
   render() {
-    return (<div>APP</div>)
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/map" component={Map}></Route> 
+          
+          <Link to="/">首页</Link>
+          <Link to="/map">地图</Link>
+        </div>
+      </Router>
+    )
   }
 }
 export default App
---
+++ src/pages/Map/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+class Map extends React.Component {
+    componentDidMount() {}
+    render() {
+        return (
+            <div>
+                Map
+            </div>
+        )
+    }
+}
+export default Map
