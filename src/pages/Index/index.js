a5dbfd20c3a42182ad6f75fe792ad8f08e17bda5
王铎 <956389596@qq.com>
2019-11-01 16:12:22 +0800

    首页-轮播图-请求

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,20 +1,53 @@
 import React from 'react'
 import { Carousel } from 'antd-mobile'
+import axios from 'axios'
+
+const BaseURL = `http://localhost:8080`
 class Index extends React.Component {
     state = {
-        data: ['1', '2', '3'],
+        swiperData: [],
         imgHeight: 176
       }
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                data: [
-                    'AiyWuByWklrrUDlFignR',
-                    'TekJlZRVCjLFexlOCuWn',
-                    'IJOtIlfsYdTyaDTRVrLI'
-                ]
-            })
-          }, 100)
+      
+    // 获取数据的方法
+    // 获取轮播图数据
+    loadSwiper = async () => {
+        const { data } = await axios.get(`http://localhost:8080/home/swiper`)
+        const { status, body } = data
+        if (status === 200) {
+            this.setState(() => {
+                return {
+                swiperData:body
+            }})
+        }
+    }
+    async componentDidMount() {
+        this.loadSwiper()
+    }
+
+    // 渲染模板的方法-------
+    // 渲染轮播图
+    renderSwiper = () => {
+        const {swiperData} = this.state
+        return swiperData.map(item => (
+            <a
+                key={item.id}
+                href="http://www.itcast.cn"
+                style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+            >
+            <img
+                src={`${BaseURL}${item.imgSrc}`}
+                alt=""
+                style={{ width: '100%', verticalAlign: 'top' }}
+                // 图片高度自适应（js的知识）
+                onLoad={() => {
+                // window event resize -> 为窗口设置resize->窗口大小改变时
+                window.dispatchEvent(new Event('resize'));
+                this.setState({ imgHeight: 'auto' });
+                }}
+            />
+            </a>
+        ))
     }
     render() {
         return (
@@ -24,27 +57,9 @@ class Index extends React.Component {
                     先写数据，再写页面
                     先写页面div+css,再写数据ajax
                 */}
-                <Carousel autoplay={true} infinite>
-                    {this.state.data.map(val => (
-                    <a
-                        key={val}
-                        href="http://www.alipay.com"
-                        style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
-                    >
-                    <img
-                        src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
-                        alt=""
-                        style={{ width: '100%', verticalAlign: 'top' }}
-                        // 图片高度自适应（js的知识）
-                        onLoad={() => {
-                        // window event resize -> 为窗口设置resize->窗口大小改变时
-                        window.dispatchEvent(new Event('resize'));
-                        this.setState({ imgHeight: 'auto' });
-                        }}
-                    />
-                    </a>
-          ))}
-        </Carousel>
+            <Carousel autoplay={true} infinite>
+                {this.renderSwiper()}
+            </Carousel>
                 {/* 导航 (4个) */}
 
                 {/* 租房小组-宫格菜单(4个) */}
